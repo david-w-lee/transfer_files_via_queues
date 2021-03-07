71dd26b5edc86e4123911fe0cc6d0c66e75a7a6b
David Lee <sea0920@hotmail.com>
2021-03-07 02:51:33 -0800

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transfer_files_via_queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
